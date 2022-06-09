e1534a21a35bbd082cfc94cbbaaa1de1ef7cf148
khs <khs@k.k>
2022-06-09 21:36:49 +0900

    테스트

---
+++ src/test/test.java
@@ -0,0 +1,7 @@
+package test;
+
+public class test {
+	public void test() {
+		System.out.println("테스트 입니다!!!");
+	}
+}
---
+++ src/test02/test02.java
@@ -0,0 +1,8 @@
+package test02;
+
+public class test02 {
+	public void test02() {
+		System.out.println("Å×½ºÆ® 22!!");
+	}
+
+}
