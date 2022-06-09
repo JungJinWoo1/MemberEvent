3e11b5811b0159dcda582953ea2e88577d23268a
JungJinWoo1 <zpokk@naver.com>
2022-06-09 21:23:18 +0900

    회원관리 이벤트 프젝

---
+++ src/Main/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	while(true) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1.회원관리 이동");
+		System.out.println("2.이벤트 관리 이동");
+		int num = sc.nextInt();
+		switch(num) {
+		case 1:break;
+		case 2:break;
+		case 3:break;
+		}break;
+	}
+}
+}
